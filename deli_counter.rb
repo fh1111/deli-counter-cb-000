7796b6b0f5bbd3f66ea5b7aaeee3e71b1b042925
Fer Hernandez <fh.1111@gmail.com>
2018-10-28 20:09:08 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,22 @@
 # Write your code here.
+
+
+def line (line)
+
+  if (line.length > 1)
+
+    string = "The line is currently: "
+
+    counter = 1
+    line.each do |customer|
+
+      string = string + counter + ". " + customer
+      counter += 1
+    end
+
+  else
+
+    puts "The line is currently empty."
+  end
+    puts string
+end
